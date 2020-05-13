9757d9dc5d92f4c030b8d65b5f2a46d2abd5a470
Amarnath <sharewithamar@gmail.com>
2020-05-13 18:43:32 +0530

    portfolio base

--- webserver/venv/server.py
+++ webserver/venv/server.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
-print(__name__)
-print()
 
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    # return 'Hello, Amar- Learning python web dev!  cool'
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 
-@app.route('/about.html')
-def about():
-    # return 'Hello, Amar- Learning python web dev!  cool'
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/blog')
-def blog():
-    return 'Amar thoughts - yoda stoic'
+def write_to_file(data):
+    with open('./venv/database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
 
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'This is my dog devil'
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2,  delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('thankyou.html')
+    else:
+        return 'soemthing went wrong. Try again!'
